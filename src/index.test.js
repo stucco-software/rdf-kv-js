fa23e5637266d59fb37630d72ed903450567749d
nikolaswise <nikolaswise@gmail.com>
2022-12-06 14:40:09 -0800

    langs and types

--- src/index.test.js
+++ src/index.test.js
@@ -50,36 +50,58 @@ describe('Convert Simple Form to RDF Triples', () => {
 			});
 	})
 
-	it('Can do both literals and nodes at the same time!', () => {
+	// Lang tags!
+	it('Can define languages', () => {
+		let formData = createFormData({
+			"dct:description @en": "A literal value in English."
+		})
+		expect(rdfkv('http://example.com/my/resource', formData))
+			.toMatchObject({
+				delete: '',
+				insert: '<http://example.com/my/resource> dct:description "A literal value in English."@en . '
+			});
+	})
+
+	// Types!
+	it('Can define data types', () => {
+		let formData = createFormData({
+			"dct:created ^xsd:date": "2022-12-01"
+		})
+		expect(rdfkv('http://example.com/my/resource', formData))
+			.toMatchObject({
+				delete: '',
+				insert: '<http://example.com/my/resource> dct:created "2022-12-01"^^xsd:date . '
+			});
+	})
+
+	// ALl together now!
+	it('Can do all of those all at once!', () => {
 		let formData = createFormData({
 			"dct:title": "Whatever you wrote",
+			"dct:description @en": "A literal value in English.",
+			"dct:created ^xsd:date": "2022-12-01",
 		  "rdf:type :": 'http://example.com/my/type'
 		})
 
 		expect(rdfkv('http://example.com/my/resource', formData))
 			.toMatchObject({
 				delete: '',
-				insert: '<http://example.com/my/resource> dct:title "Whatever you wrote" . <http://example.com/my/resource> rdf:type <http://example.com/my/type> . '
+				insert: '<http://example.com/my/resource> dct:title "Whatever you wrote" . <http://example.com/my/resource> dct:description "A literal value in English."@en . <http://example.com/my/resource> dct:created "2022-12-01"^^xsd:date . <http://example.com/my/resource> rdf:type <http://example.com/my/type> . '
 			});
 	})
 });
 
 
 
-// refernce a node:
-// <input name="http://www.w3.org/1999/02/22-rdf-syntax-ns#type :"/>
-
-// reference a blank node:
-// <input name="http://www.w3.org/1999/02/22-rdf-syntax-ns#type _"/>
-
-// lit, lang, and types
-// <input name="http://purl.org/dc/terms/description @en"/>
 // <input name="http://purl.org/dc/terms/created
 //              ^http://www.w3.org/2001/XMLSchema#date"/>
 
 // For the sake of completeness, although it likely won’t come
 // up often in practice, the character to disambiguate plain
 // literals is the apostrophe '
+
+// reference a blank node:
+// <input name="http://www.w3.org/1999/02/22-rdf-syntax-ns#type _"/>
 
 // subjects
 // <form method="POST" action="http://example.com/my/resource">
--- src/lib/index.js
+++ src/lib/index.js
@@ -2,15 +2,26 @@
 export const add = (x, y) => x + y
 
 const objectIsResource = (predicate) => predicate.endsWith(' :')
-
+const objectHasLang = (predicate) => predicate.includes(' @')
+const objectHasType = (predicate) => predicate.includes(' ^')
 
 const reducer = (subject) => (acc, cur, i) => {
   let out
   let predicate
+  let lang
+  let type
   switch (true) {
     case objectIsResource(cur[0]):
       predicate = cur[0].split(' :')[0]
       return `${acc}<${subject}> ${predicate} <${cur[1]}> . `
+    case objectHasLang(cur[0]):
+      predicate = cur[0].split(' @')[0]
+      lang = cur[0].split(' @')[1]
+      return `${acc}<${subject}> ${predicate} "${cur[1]}"@${lang} . `
+    case objectHasType(cur[0]):
+      predicate = cur[0].split(' ^')[0]
+      type = cur[0].split(' ^')[1]
+      return `${acc}<${subject}> ${predicate} "${cur[1]}"^^${type} . `
     case !objectIsResource(cur[0]):
       predicate = cur[0]
       return `${acc}<${subject}> ${predicate} "${cur[1]}" . `
